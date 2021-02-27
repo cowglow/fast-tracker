1f1ffb36258aa2d2a2b373ea7d758deb33fc8ea3
cowglow <cowglow@gmail.com>
2021-02-27 17:24:47 +0100

    Add app state context

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import withStyles from "./app.style";
+import { useAppState } from "../../context/app-state-context";
 
 interface AppProps {
   classes: any;
 }
 
 const App: React.FC<AppProps> = ({ classes }) => {
+  const fastConfiguration = useAppState();
   return (
     <div className={classes.root}>
       <header className={classes.header}>header goes here</header>
-      <main className={classes.main}>main goes here</main>
+      <main className={classes.main}>
+        <h1>main goes here</h1>
+        <pre>{JSON.stringify(fastConfiguration, null, 2)}</pre>
+      </main>
       <footer className={classes.footer}>footer goes here</footer>
     </div>
   );
