e3770475c0e444dc9f387058a648426c012ee266
cowglow <cowglow@gmail.com>
2021-04-09 01:39:49 +0200

    Adds decorator to app story

--- src/components/app/app.story.tsx
+++ src/components/app/app.story.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import App from "../app/app";
+import theme from "../../service/theme";
+import { BrowserRouter } from "react-router-dom";
+import { AppStateProvider } from "../../context/app-state-context";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Meta } from "@storybook/react";
 
 export default {
+  component: App,
   title: "Web App",
-  component: <App />,
-};
+  decorators: [
+    (Story: any) => (
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AppStateProvider>
+            <CssBaseline />
+            <Story />
+          </AppStateProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    ),
+  ],
+} as Meta;
+
+export const AppStory = () => <App />;
