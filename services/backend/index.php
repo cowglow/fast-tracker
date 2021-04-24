95e4090fe80b2ac471d2a9a6fbd7e9201f4b7dc2
cowglow <cowglow@gmail.com>
2021-04-24 22:45:52 +0200

    WIP: mysqli connection not working

--- services/backend/index.php
+++ services/backend/index.php
@@ -5,7 +5,18 @@
 
 //$output = new Cowglow\Backend\Client($_ENV["SECRET_KEY"]);
 
-header("Content-Type: application/json; charset=UTF-8");
+$HOST = 'db';
+$USER = 'user';
+$PASSWORD = 'user';
+$DATABASE = "fast-tracking-db";
+$PORT = 3306;
 
-echo json_encode(['foo'=>'bar']);
+$connection = new mysqli($HOST, $USER, $PASSWORD, $DATABASE, $PORT);
+$message = "Successfully connected to MySQL";
+if($connection->connect_error){
+    $message = "Connection Failed: " . $connection->connect_error;
+}
+
+//header("Content-Type: application/json; charset=UTF-8");
+echo json_encode(['foobar' => $message]);
 //echo json_encode($output->Render());
